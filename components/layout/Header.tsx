93da8a81cd16b5955949aabf29f7233483ef5b4d
Valentine Irungu <irunguvalentine73@gmail.com>
2025-06-10 09:29:28 +0300

    Update Header DropDown

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -26,7 +26,6 @@ const Header: React.FC = () => {
                             viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={menuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'} />
-
                         </svg>
                     </button>
 
@@ -55,7 +54,7 @@ const Header: React.FC = () => {
                                     aria-label="Close Menu"
                                 >
                                     <svg
-                                        className='w-6 h-6'
+                                        className='w-6 h-6 border border-white rounded-full p-1'
                                         fill="none"
                                         viewBox="0 0 24 24"
                                         stroke="currentColor"
