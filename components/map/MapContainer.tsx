ea5bd7f7e91cbe3e7205a7431c96a8d78ff39b7a
Valentine Irungu <irunguvalentine73@gmail.com>
2025-06-13 06:19:56 +0300

    Add toggle feature to MapLayer

--- components/map/MapContainer.tsx
+++ components/map/MapContainer.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { MapContainer as LeafletMap, TileLayer, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Layers } from 'lucide-react';
 import { MapPosition, MapContainerProps, LayerVisibility } from '@/interfaces';
 import { DEFAULT_MAP_POSITION, MAP_TILE_LAYERS, EE_TILE_LAYERS } from '@/components/utils/constants';
 import LayerControls from './LayerControls';
@@ -28,6 +29,8 @@ const MapContainer: React.FC<MapContainerProps> = () => {
         uhi: false,
         reports: true,
     });
+
+    const [controlsOpen, setControlsOpen] = useState(false);
 
     const handleLayerToggle = (layer: keyof LayerVisibility) => {
         setLayerVisibility(prev => ({
@@ -61,7 +64,7 @@ const MapContainer: React.FC<MapContainerProps> = () => {
                     <TileLayer
                         url={EE_TILE_LAYERS.ndvi.url}
                         attribution={EE_TILE_LAYERS.ndvi.attribution}
-                        opacity={0.7}
+                        opacity={0.6}
                     />
                 )}
 
@@ -77,12 +80,41 @@ const MapContainer: React.FC<MapContainerProps> = () => {
 
                 {/* Controls */}
                 <div className="absolute top-4 right-4 z-[1000] flex flex-col gap-4">
-                    <LayerControls
-                        layerVisibility={layerVisibility}
-                        onLayerToggle={handleLayerToggle}
-                        mapType={mapType}
-                        onMapTypeChange={handleMapTypeChange}
-                    />
+                    {/* Toogle Button (Show when closed) */}
+                    {!controlsOpen && (
+                        <button
+                            className='bg-white rounded-full shadow p-2 border border-gray-200'
+                            onClick={() => setControlsOpen(true)}
+                            aria-label='Show Layer Controls'
+                        >
+                            <Layers className='h-6 w-6  text-emerald-700' />
+
+                        </button>
+                    )}
+
+                    {/* Layer Controls (Show when open) */}
+                    <div
+                        className={`transition-transform duration-300 ease-in-out ${controlsOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-o pointer-events-none'}`}
+                    >
+                        {controlsOpen && (
+                            <div className='relative'>
+                                {/* Close Button (Layers Icon) */}
+                                <button
+                                    className='absolute right-4 top-2 bg-white rounded-full shadow p-2 border border-gray-200'
+                                    onClick={() => setControlsOpen(false)}
+                                    aria-label='Hide Layer Controls'
+                                >
+                                    <Layers className='h-5 w-5 text-emerald-700' />
+                                </button>
+                                <LayerControls
+                                    layerVisibility={layerVisibility}
+                                    onLayerToggle={handleLayerToggle}
+                                    mapType={mapType}
+                                    onMapTypeChange={handleMapTypeChange}
+                                />
+                            </div>
+                        )}
+                    </div>
                 </div>
 
                 <div className="absolute bottom-4 right-4 z-[1000]">
@@ -94,16 +126,18 @@ const MapContainer: React.FC<MapContainerProps> = () => {
                 {layerVisibility.ndvi && (
                     <div className="absolute bottom-4 left-4 bg-white p-2 rounded shadow z-[1000] text-xs">
                         <strong>NDVI Legend</strong><br />
-                        <span className="legend-square-red">■</span> Low <br />
-                        <span className="legend-square-green">■</span> High
+                        <span className="legend-square-red">■</span> Low (0.0 – 0.2): Bare/urban land <br />
+                        <span className="legend-square-yellow">■</span> Moderate (0.2 – 0.4): Sparse vegetation <br />
+                        <span className="legend-square-green">■</span> High (0.4 – 0.8): Dense vegetation
                     </div>
                 )}
 
                 {layerVisibility.uhi && (
-                    <div className="absolute bottom-24 left-4 bg-white p-2 rounded shadow z-[1000] text-xs">
-                        <strong>LST Legend</strong><br />
-                        <span className="legend-square-blue">■</span> Cool <br />
-                        <span className="legend-square-red">■</span> Hot
+                    <div className="absolute bottom-27 left-4 bg-white p-2 rounded shadow z-[1000] text-xs">
+                        <strong>LST (°C) Legend</strong><br />
+                        <span className="legend-square-blue">■</span> Cool (25 – 30°C) <br />
+                        <span className="legend-square-yellow">■</span> Warm (30 – 38°C) <br />
+                        <span className="legend-square-red">■</span> Hot (38 – 45°C)
                     </div>
                 )}
 
